301ce76920bfa28c58194c746f4aa9e2fcf498e6
Gayangamage <gayangamage.se@gmail.com>
2022-11-03 08:24:20 +0100

    feat:creat cart item

--- src/components/cart-dropdown/CartDropdown.component.jsx
+++ src/components/cart-dropdown/CartDropdown.component.jsx
@@ -1,10 +1,19 @@
 import "./cartDropdown.style.scss";
 import Button from "../button/Button";
+import CartItem from "../cart-item/CartItem.component";
+import { useContext } from "react";
+import { CartContext } from "../../context/Cart.context";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} CartItem={item} />
+        ))}
+      </div>
       <Button>CHECKOUT</Button>
     </div>
   );
