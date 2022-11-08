9dbb41185a18adc7e07c4d70c4005c8ea8755d11
3GRuby <gayangamage.me@gmail.com>
2022-11-08 14:37:19 +0100

    feat: add routes for shop page panel

--- src/components/directory-item/DirectoryItem.components.jsx
+++ src/components/directory-item/DirectoryItem.components.jsx
@@ -1,10 +1,14 @@
 import "./DirectoryItem.styles.scss";
 import React from "react";
+import { useNavigate } from "react-router";
 
 const DirectroyItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate()
+
+  const onNavigateHandler = () => navigate(route)
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler} >
       <div
         className="background-image"
         style={{
